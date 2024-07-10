1523a4fa18cf52124f9ba169e8ab4246a7cb3644
alex-lange <lange.alexander222@gmail.com>
2024-07-10 13:16:07 -0400

    some updates/documentation in mesh.lua based on my understandings and preferences

--- mesh.lua
+++ mesh.lua
@@ -1,7 +1,8 @@
 --[[
 Wrapper around Delaunator's Delaunay triangulation and its dual Voronoi diagram.
 
-This is based on Amit Patel's amazing writing on Delaunay triangles, Vornoi cells, and polygonal map generation, particularly:
+This is based on Amit Patel's amazing writing on Delaunay triangles, Vornoi cells, and polygonal
+map generation, particularly:
 
 "Delaunator Guide"
 https://mapbox.github.io/delaunator/
@@ -9,62 +10,89 @@ https://mapbox.github.io/delaunator/
 "Data structure for triangle meshes"
 https://www.redblobgames.com/x/1722-b-rep-triangle-meshes/
 
-This code is mostly a Lua port of the JavaScript code from the above articles and their dual-mesh repository:
+Their dual-mesh repository which implements the above ideas:
 https://github.com/redblobgames/dual-mesh/blob/c26618aec07af7e14b2c8ebbb97cd137c3f06c18/index.js
+
+Licence from https://github.com/redblobgames/dual-mesh
+Copyright 2017 Red Blob Games <redblobgames@gmail.com>
+License: Apache v2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
 ]]
 
 --[[
-From the dual-mesh repository and source code:
+This is the summary of my understanding of the above resources:
 
-From https://github.com/redblobgames/dual-mesh
-Copyright 2017 Red Blob Games <redblobgames@gmail.com>
-License: Apache v2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
+This Mesh class represents a Delaunay triangulation and its dual Voronoi diagram with minimal
+amount of data. The data we need to store is:
+  - the voronoi cells (c)
+  - the delaunay triangles (t)
+  - the "edges" (e), each representing both an edge of a triangle and an edge of a cell.
+
+Each element has an id:
+  - 0 <= c < numCells
+  - 0 <= t < numTriangles
+  - 0 <= e < numEdges
+
+We can represent both types of edges with the same table because each triangle edge
+intersects with exactly one cell edge:
+
+Delaunay Triangulation:          Voronoi Cell:
+     .         .                  .         .
+    / \       / \                / \  /O\  / \
+   /   \     /   \              /   \/   \/   \
+  /     \   /     \            /   O/\   /\O   \
+ /_______\ /_______\          /____|__\ /__|____\
+ \       / \       /          \    |  / \  |    /
+  \     /   \     /            \   O\/   \/O   /
+   \   /     \   /              \   /\   /\   /
+    \ /       \ /                \ /  \O/  \ /
+     .         .                  .         .
+
+Each Cell contains exactly one Triangle point (i.e. one seed point of the triangluation), and each
+Triangle contains exactly one Cell point (defined as the center/centroid of the Triangle). We store
+these points in the _cellPositions and _trianglePositions tables, respectively.
+
+Delaunator stores each edge as two half-edges. This is so that each triangle has a unique half-edge,
+and that each half-edge can be identified by a single seed point (and also resulting in the number
+of edges being the same as the number of cells and the number of triangles!).
+The neighboring triangle has the opposite half-edge.
+  - delaunator.triangles[e] contains the index of a point where the half-edge of a triangle starts,
+    and therefore this point is also the position of a cell that intersects with this triangle.
+  - the half-edges of a triangle are indexed consecutively, so the three points of a triangle are:
+      delaunator.triangles[e], delaunator.triangles[e + 1], delaunator.triangles[e + 2]
+  - for half-edge e, the triangleOfEdge(e) connects two seed points (/ cell positions), delaunay.triangles[e] and delaunay.triangles[nextHalfedge(e)]
+
+  - delaunator.halfEdges[e] contains the index of the opposite half-edge of the neighboring triangle
+  - the cell'a half-edge connects two cell points, which are the centers of triangleOfEdge(e) and triangleOfEdge(delaunay.halfedges[e])
 ]]
 
 local Class = require((...):gsub("mesh", "class"))
 
 local Mesh = Class {}
 
-local function triangleFromSide(side)
+local function triangleOfEdge(side)
   return math.floor(side / 3)
 end
 
-local function previousSide(side)
-  if side % 3 == 0 then
-    return side + 2
-  else
-    return side - 1
-  end
-end
-
-local function nextSide(side)
-  if side % 3 == 2 then
-    return side - 2
-  else
-    return side + 1
-  end
-end
-
 function Mesh:init(points, delaunator)
-  self.cellVertices = {}
+  self._cellPositions = {}
   -- shift points from index-1 to index-0
   for i = 1, #points do
-    self.cellVertices[i - 1] = { x = points[i].x, y = points[i].y }
+    self._cellPositions[i - 1] = { x = points[i].x, y = points[i].y }
   end
 
   self._triangles = delaunator.triangles
   self._halfEdges = delaunator.halfEdges
 
-  self.numSides = #self._triangles + 1 -- add 1 because Lua indexes by 1 not 0 so it doesn't got the 0 index as part of the length
-  if self.numSides % 3 ~= 0 then
-    error("Invalid number of sides is not divisble by 3: " .. self.numSides)
+  self.numEdges = #self._triangles + 1 -- add 1 because Lua indexes by 1 not 0 so it doesn't got the 0 index as part of the length
+  if self.numEdges % 3 ~= 0 then
+    error("Invalid number of sides is not divisble by 3: " .. self.numEdges)
   end
-  self.numCells = #self.cellVertices + 1
-  self.numTriangles = self.numSides / 3
+  self.numCells = #self._cellPositions + 1
+  self.numTriangles = self.numEdges / 3
 
-  self.triangleVertices = {}
+  self._trianglePositions = {}
   for i = 0, self.numTriangles - 1 do
-    self.triangleVertices[i] = { x = 0 , y = 0 }
+    self._trianglePositions[i] = { x = 0 , y = 0 }
   end
   log.info("max i = " .. self.numTriangles - 1)
 end
@@ -72,87 +100,96 @@ end
 function Mesh:load()
   -- Construct an index for finding sides connected to a cell
   self.sideOfCell = {}
-  for s = 0, self.numSides - 1 do
-    local endpoint = self._triangles[nextSide(s)]
-    if (self.sideOfCell[endpoint] == nil or self._halfEdges[s] == -1) then
-      self.sideOfCell[endpoint] = s
+  for c = 0, self.numEdges - 1 do
+    local endpoint = self._triangles[self:nextHalfEdge(c)]
+    if (self.sideOfCell[endpoint] == nil or self._halfEdges[c] == -1) then
+      self.sideOfCell[endpoint] = c
     end
   end
 
   -- Construct triangle coordinates
-  for s = 0, self.numSides - 1, 3 do
+  for s = 0, self.numEdges - 1, 3 do
     local t = math.floor(s / 3)
-    local a = self.cellVertices[self._triangles[s]]
-    local b = self.cellVertices[self._triangles[s + 1]]
-    local c = self.cellVertices[self._triangles[s + 2]]
+    local a = self._cellPositions[self._triangles[s]]
+    local b = self._cellPositions[self._triangles[s + 1]]
+    local c = self._cellPositions[self._triangles[s + 2]]
 
     -- TODO: Check if ghost
     -- ghost triangle center is just outside the unpaired side
     -- solid triangle center is at the centroid
-    self.triangleVertices[t].x = (a.x + b.x + c.x) / 3
-    self.triangleVertices[t].y = (a.y + b.y + c.y) / 3
+    self._trianglePositions[t].x = (a.x + b.x + c.x) / 3
+    self._trianglePositions[t].y = (a.y + b.y + c.y) / 3
   end
 end
 
 function Mesh:cellPosition(cell)
-  return self.cellVertices[cell]
+  return self._cellPositions[cell]
 end
 
 function Mesh:trianglePosition(triangle)
-  return self.triangleVertices[triangle]
+  return self._trianglePositions[triangle]
 end
 
+-- Given a side of a triangle, return the next side of that same triangle.
+-- (0 -> 1, 1 -> 2, 2 -> 0)
+function Mesh:nextHalfEdge(edge)
+  if edge % 3 == 2 then
+    return edge - 2
+  else
+    return edge + 1
+  end
+end
+
+-- Given a side of a triangle, return the previous side of that same triangle.
+-- (0 -> 2, 2 -> 1, 1 -> 0)
+function Mesh:previousHalfEdge(edge)
+  if edge % 3 == 0 then
+    return edge + 2
+  else
+    return edge - 1
+  end
+end
 
 -- A side is directed. If two triangles t0, t1 are adjacent, there will
 -- be two sides representing the boundary, one for t0 and one for t1. These
 -- can be accessed with triangleWithInnerSide and triangleWithOuterSide.
 function Mesh:triangleWithInnerSide(s)
-  return triangleFromSide(s)
+  return triangleOfEdge(s)
 end
 
 function Mesh:triangleWithOuterSide(s)
-  return triangleFromSide(self._halfEdges[s])
+  return triangleOfEdge(self._halfEdges[s])
 end
 
 -- A side also represents the boundary between two cells. If two cells
--- r0, r1 are adjacent, there will be two sides representing the boundary,
--- cellWithBeginningSide and cellWithEndingSide.
-function Mesh:cellWithBeginningSide(s)
+-- are adjacent, there will be two sides representing the boundary,
+-- cellWithInnerSide and cellWithOuterSide.
+function Mesh:cellWithInnerSide(s)
   return self._triangles[s]
 end
 
-function Mesh:cellWithEndingSide(s)
-  return self._triangles[nextSide(s)]
-end
-
-
-function Mesh:nextSide(s)
-  return nextSide(s)
-end
-
-function Mesh:previousSide(s)
-  return previousSide(s)
+function Mesh:cellWithOuterSide(s)
+  return self._triangles[self:nextHalfEdge(s)]
 end
 
 -- A side from p-->q will have a pair q-->p, at index
--- s_opposite_s. It will be -1 if the side doesn't have a pair.
+-- oppositeSide(side). It will be -1 if the side doesn't have a pair.
 -- Use addGhostStructure() to add ghost pairs to all sides.
-
 function Mesh:oppositeSide(s)
   return self._halfEdges[s]
 end
 
-function Mesh:sidesAroundTriangle(t)
+function Mesh:edgesOfTriangle(t)
   return t * 3, t * 3 + 1, t * 3 + 2
 end
 
 function Mesh:cellsAroundTriangle(t)
-  local a, b, c = self:sidesAroundTriangle(t)
+  local a, b, c = self:edgesOfTriangle(t)
   return { self._triangles[a], self._triangles[b], self._triangles[c] }
 end
 
 function Mesh:trianglesAroundTriangle(t)
-  local a, b, c = self:sidesAroundTriangle(t)
+  local a, b, c = self:edgesOfTriangle(t)
   return { self:triangleWithOuterSide(a), self:triangleWithOuterSide(b), self:triangleWithOuterSide(c) }
 end
 
@@ -167,7 +204,7 @@ function Mesh:sidesAroundCell(r)
 
   while true do
     table.insert(sides, self:oppositeSide(incoming))
-    local outgoing = self:nextSide(incoming)
+    local outgoing = self:nextHalfEdge(incoming)
     incoming = self:oppositeSide(outgoing)
     if incoming == -1 or incoming == s or incoming == nil then
       break
@@ -180,7 +217,7 @@ function Mesh:sidePositionsAroundCell(c)
   local sides = self:sidesAroundCell(c)
   local positions = {}
   for _, s in ipairs(sides) do
-    local t = triangleFromSide(s)
+    local t = self:triangleWithInnerSide(s)
     table.insert(positions, self:trianglePosition(t))
   end
   return positions
@@ -191,8 +228,8 @@ function Mesh:cellsAroundCell(r)
   local incoming = s
   local cells = {}
   while true do
-    table.insert(cells, self:cellWithBeginningSide(incoming))
-    local outgoing = self:nextSide(incoming)
+    table.insert(cells, self:cellWithInnerSide(incoming))
+    local outgoing = self:nextHalfEdge(incoming)
     incoming = self:oppositeSide(outgoing)
     if incoming == -1 or incoming == s then
       break
@@ -205,8 +242,8 @@ function Mesh:trianglesAroundCell(r)
   local incoming = s
   local triangles = {}
   while true do
-    table.insert(triangles, self:triangleFromSide(incoming))
-    local outgoing = self:nextSide(incoming)
+    table.insert(triangles, self:triangleWithInnerSide(incoming))
+    local outgoing = self:nextHalfEdge(incoming)
     incoming = self:oppositeSide(outgoing)
     if incoming == -1 or incoming == s then
       break
@@ -215,21 +252,26 @@ function Mesh:trianglesAroundCell(r)
   return triangles
 end
 
+function Mesh:pointsOfCellSide(e)
+  local p1 = self:trianglePosition(self:triangleWithInnerSide(e))
+  local p2 = self:trianglePosition(self:triangleWithOuterSide(e))
+  return p1, p2
+end
+
 function Mesh:forEachCellEdge(callback)
-  for e = 0, self.numSides - 1 do
+  for e = 0, self.numEdges - 1 do
     if (e < self._halfEdges[e]) then
-      local p1 = self:trianglePosition(self:triangleWithInnerSide(e))
-      local p2 = self:trianglePosition(self:triangleWithOuterSide(e))
+      local p1, p2 = self:pointsOfCellSide(e)
       callback(p1, p2)
     end
   end
 end
 
 function Mesh:forEachTriangleEdge(callback)
-  for e = 0, self.numSides - 1 do
+  for e = 0, self.numEdges - 1 do
     if (e < self._halfEdges[e]) then
       local p1 = self:cellPosition(self._triangles[e])
-      local p2 = self:cellPosition(self._triangles[self:nextSide(e)])
+      local p2 = self:cellPosition(self._triangles[self:nextHalfEdge(e)])
       if p1 and p2 then
         callback(p1, p2)
       end
